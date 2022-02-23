8135f4edef2d3ee6a09913b02fe93b599d0ca296
epent <pentium95@mail.ru>
2022-02-23 18:59:12 +0200

    add .env file

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 
 const app = express();
+const port = process.env.PORT || 8080;
 
-app.listen(8080);
+app.get("/", (req, res, next) => {
+  res.send("Hello world!!!");
+});
+
+app.listen(port);
