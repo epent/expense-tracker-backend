79e5ce56b64876fa43f6c5570a5ad288d2d330ac
epent <pentium95@mail.ru>
2022-02-24 11:47:30 +0200

    est connection with db through sequalize

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const sequelize = require("./util/database");
+
 const app = express();
 const port = process.env.PORT || 8080;
 
@@ -7,4 +9,14 @@ app.get("/", (req, res, next) => {
   res.send("Hello world!!!");
 });
 
-app.listen(port);
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .then(() => {
+    app.listen(port);
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
---
+++ util/database.js
@@ -0,0 +1,11 @@
+const Sequelize = require("sequelize");
+
+const { DB_HOST, DB_USERNAME, DB_PASSWORD } = process.env;
+
+const sequelize = new Sequelize("expense_tracker", DB_USERNAME, DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: "postgres",
+  operatorsAliases: false,
+});
+
+module.exports = sequelize;
