e475cd97ff252660affff95709e61044c4563f09
epent <pentium95@mail.ru>
2022-02-24 18:14:22 +0200

    add postCategory controller

--- controllers/accountcategory.js
+++ controllers/accountcategory.js
@@ -1,4 +1,5 @@
 const Account = require("../models/account");
+const Category = require("../models/category");
 
 exports.postAccount = async (req, res, next) => {
   console.log(req.body);
@@ -14,6 +15,23 @@ exports.postAccount = async (req, res, next) => {
     });
 
     res.status(201).json({ account: account });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.postCategory = async (req, res, next) => {
+  console.log(req.body);
+  try {
+    const name = req.body.Name;
+    const balance = req.body.Balance;
+
+    const category = await Category.create({
+      name: name,
+      balance: balance,
+    });
+
+    res.status(201).json({ category: category });
   } catch (error) {
     console.log(error);
   }
