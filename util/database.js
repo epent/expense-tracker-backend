2f5c712b32104511353f2d2bcfbf493d823edf81
epent <pentium95@mail.ru>
2022-02-24 18:16:56 +0200

    update db definition

--- util/database.js
+++ util/database.js
@@ -1,11 +1,10 @@
-const Sequelize = require("sequelize");
+const { Sequelize } = require("sequelize");
 
 const { DB_HOST, DB_USERNAME, DB_PASSWORD } = process.env;
 
 const sequelize = new Sequelize("expense_tracker", DB_USERNAME, DB_PASSWORD, {
   host: DB_HOST,
   dialect: "postgres",
-  operatorsAliases: false,
 });
 
 module.exports = sequelize;
