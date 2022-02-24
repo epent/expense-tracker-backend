1aa4da92e824b32c9c75fc933e1df94312a2c4a2
epent <pentium95@mail.ru>
2022-02-24 15:11:23 +0200

    add associations

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 
 const sequelize = require("./util/database");
+const Account = require("./models/account");
+const Category = require("./models/category");
+const Expense = require("./models/expense");
+const Income = require("./models/income");
+const Transfer = require("./models/transfer");
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -9,13 +14,44 @@ app.get("/", (req, res, next) => {
   res.send("Hello world!!!");
 });
 
+Category.hasMany(Expense, {
+  foreignKey: {
+    allowNull: false,
+  },
+});
+Expense.belongsTo(Category);
+
+Account.hasMany(Expense, {
+  foreignKey: {
+    allowNull: false,
+  },
+});
+Expense.belongsTo(Account);
+
+Account.hasMany(Income, {
+  foreignKey: {
+    allowNull: false,
+  },
+});
+Income.belongsTo(Account);
+
+Account.hasMany(Transfer, {
+  foreignKey: {
+    allowNull: false,
+  },
+});
+Transfer.belongsTo(Account);
+
 sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Connection has been established successfully.");
+  .sync({ force: true })
+  .then((result) => {
+    app.listen(port);
   })
   .then(() => {
-    app.listen(port);
+    Account.create({ name: "Visa", category: "Creadit card", balance: 100 });
+  })
+  .then(() => {
+    console.log(sequelize.models);
   })
   .catch((err) => {
     console.error("Unable to connect to the database:", err);
