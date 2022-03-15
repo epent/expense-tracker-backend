64c30ab31d7f7baa2dcec9e761296708faab78a7
epent <pentium95@mail.ru>
2022-03-15 19:21:27 +0200

    add expenses test: POST, +/-

--- tests/transaction.test.js
+++ tests/transaction.test.js
@@ -1,6 +1,5 @@
 const request = require("supertest");
-
-const { v4: uuidv4 } = require("uuid");
+const db = require("../db/models");
 
 const app = require("../app");
 
@@ -54,6 +53,114 @@ describe("GET /transfers", () => {
       const res = await request(app).get("/transfers");
 
       expect(res.body.length).toBe(0);
+    });
+  });
+});
+
+describe("POST /expense", () => {
+  describe("positive tests", () => {
+    test("should respond with status 201, expense body", async () => {
+      const res = await request(app).post("/expense").send({
+        Amount: 10,
+        Date: new Date(),
+        From: "Visa",
+        To: "Food",
+      });
+
+      expect(res.statusCode).toEqual(201);
+      expect(res.body).toHaveProperty("expense");
+    });
+
+    test("should increase expenses length", async () => {
+      const before = await request(app).get("/expenses");
+
+      await request(app).post("/expense").send({
+        Amount: 10,
+        Date: new Date(),
+        From: "Visa",
+        To: "Food",
+      });
+
+      const after = await request(app).get("/expenses");
+
+      expect(after.body.length).toBe(before.body.length + 1);
+    });
+
+    test("should update total and expenses Balance", async () => {
+      const before = await request(app).get("/balances");
+
+      const req = {
+        Amount: 10,
+        Date: new Date(),
+        From: "Visa",
+        To: "Food",
+      };
+
+      await request(app).post("/expense").send(req);
+
+      const after = await request(app).get("/balances");
+
+      expect(after.body.total).toBe(before.body.total - req.Amount);
+      expect(after.body.expenses).toBe(before.body.expenses + req.Amount);
+    });
+
+    test("should update Account balance", async () => {
+      const req = {
+        Amount: 10,
+        Date: new Date(),
+        From: "Visa",
+        To: "Food",
+      };
+      const before = await db.account.findByPk(req.From);
+
+      await request(app).post("/expense").send(req);
+
+      const after = await db.account.findByPk(req.From);
+
+      expect(after.dataValues.balance).toBe(
+        before.dataValues.balance - req.Amount
+      );
+    });
+
+    test("should update Category balance", async () => {
+      const req = {
+        Amount: 10,
+        Date: new Date(),
+        From: "Visa",
+        To: "Food",
+      };
+      const before = await db.category.findByPk(req.To);
+
+      await request(app).post("/expense").send(req);
+
+      const after = await db.category.findByPk(req.To);
+
+      expect(after.dataValues.balance).toBe(
+        before.dataValues.balance + req.Amount
+      );
+    });
+  });
+
+  describe("negative tests", () => {
+    test("should respond with status 422 - input is empty string", async () => {
+      const res = await request(app).post("/expense").send({
+        Amount: 10,
+        Date: new Date(),
+        From: "",
+        To: "Food",
+      });
+
+      expect(res.statusCode).toEqual(422);
+    });
+
+    test("should respond with status 422 - input is missing", async () => {
+      const res = await request(app).post("/expense").send({
+        Amount: 10,
+        Date: new Date(),
+        From: "Visa",
+      });
+
+      expect(res.statusCode).toEqual(422);
     });
   });
 });
