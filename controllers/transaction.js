7f8f36937ae539adc62157b8ebe33db3f40a3496
epent <pentium95@mail.ru>
2022-03-15 19:34:24 +0200

    add errors if input is missing (income)

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -60,6 +60,19 @@ exports.postExpense = async (req, res, next) => {
 
 exports.postIncome = async (req, res, next) => {
   try {
+    Object.keys(req.body).forEach((key) => {
+      if (req.body[key] === "") {
+        const error = new Error("Input is empty string");
+        error.statusCode = 422;
+        throw error;
+      }
+    });
+    if (Object.keys(req.body).length !== 4) {
+      const error = new Error("Input is missing");
+      error.statusCode = 422;
+      throw error;
+    }
+
     const from = req.body.From;
     const account = req.body.To;
     const amount = req.body.Amount;
