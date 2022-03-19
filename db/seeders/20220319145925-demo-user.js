c7a560d620126431128ff290c8694db449093bd7
epent <pentium95@mail.ru>
2022-03-19 17:06:20 +0200

    add demo-user Seed

---
+++ db/seeders/20220319145925-demo-user.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const { v4: uuidv4 } = require("uuid");
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          id: uuidv4(),
+          firstName: "John",
+          lastName: "Doe",
+          email: "test@test.com",
+          password: "testpassword",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("users", null, {});
+  },
+};
