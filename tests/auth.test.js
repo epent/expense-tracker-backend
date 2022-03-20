c0570b8ddb71aeda1db0f8ff100f93c99192f611
epent <pentium95@mail.ru>
2022-03-20 15:11:54 +0200

    add login test: POST, +/-

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -22,7 +22,7 @@ describe("POST /signup", () => {
       await request(app).post("/signup").send({
         FirstName: "Test",
         LastName: "Test",
-        Email: "unique@mail.com",
+        Email: "notunique@mail.com",
         Password: "1234567",
       });
 
@@ -34,6 +34,40 @@ describe("POST /signup", () => {
       });
 
       expect(res.statusCode).toEqual(422);
+    });
+  });
+});
+
+describe("POST /login", () => {
+  describe("positive tests", () => {
+    test("should respond with status 200, user body", async () => {
+      const res = await request(app).post("/login").send({
+        Email: "unique@user.com",
+        Password: "1234567",
+      });
+
+      expect(res.statusCode).toEqual(200);
+      expect(res.body).toHaveProperty("user");
+    });
+  });
+
+  describe("negative tests", () => {
+    test("should respond with status 401 - wrong email", async () => {
+      const res = await request(app).post("/login").send({
+        Email: "unique@user.com",
+        Password: "wrongemail",
+      });
+
+      expect(res.statusCode).toEqual(401);
+    });
+
+    test("should respond with status 401 - wrong password", async () => {
+      const res = await request(app).post("/login").send({
+        Email: "wrong@user.com",
+        Password: "1234567",
+      });
+
+      expect(res.statusCode).toEqual(401);
     });
   });
 });
