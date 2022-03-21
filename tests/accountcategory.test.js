d9ff4c2f7401666cd061a54a9caac9f40a70c36c
epent <pentium95@mail.ru>
2022-03-21 20:02:30 +0200

    tests: set auth token in accounts

--- tests/accountcategory.test.js
+++ tests/accountcategory.test.js
@@ -5,17 +5,34 @@ const { v4: uuidv4 } = require("uuid");
 const app = require("../app");
 
 describe("GET /accounts", () => {
+  let token;
+  beforeAll(async () => {
+    const res = await request(app).post("/login").send({
+      Email: "unique@user.com",
+      Password: "1234567",
+    });
+    token = res.body.token;
+  });
+
   describe("positive tests", () => {
     test("should respond with status 200", async () => {
-      await request(app).get("/accounts").expect(200);
+      await request(app)
+        .get("/accounts")
+        .set("Authorization", `Bearer ${token}`)
+        .expect(200);
     });
 
     test("should respond with json type", async () => {
-      await request(app).get("/accounts").expect("Content-Type", /json/);
+      await request(app)
+        .get("/accounts")
+        .set("Authorization", `Bearer ${token}`)
+        .expect("Content-Type", /json/);
     });
 
     test("should respond with 4 accounts", async () => {
-      const res = await request(app).get("/accounts");
+      const res = await request(app)
+        .get("/accounts")
+        .set("Authorization", `Bearer ${token}`);
 
       expect(res.body.length).toBe(4);
     });
@@ -41,34 +58,55 @@ describe("GET /categories", () => {
 });
 
 describe("POST /account", () => {
+  let token;
+  beforeAll(async () => {
+    const res = await request(app).post("/login").send({
+      Email: "unique@user.com",
+      Password: "1234567",
+    });
+    token = res.body.token;
+  });
+
   describe("positive tests", () => {
     test("should respond with status 201, account body", async () => {
-      const res = await request(app).post("/account").send({
-        Name: "New 1",
-        Category: "Bank account",
-        Balance: 100,
-      });
+      const res = await request(app)
+        .post("/account")
+        .set("Authorization", `Bearer ${token}`)
+        .send({
+          Name: "New 1",
+          Category: "Bank account",
+          Balance: 100,
+        });
 
       expect(res.statusCode).toEqual(201);
       expect(res.body).toHaveProperty("account");
     });
 
     test("should increase accounts length", async () => {
-      const before = await request(app).get("/accounts");
+      const before = await request(app)
+        .get("/accounts")
+        .set("Authorization", `Bearer ${token}`);
 
-      await request(app).post("/account").send({
-        Name: "New 2",
-        Category: "Bank account",
-        Balance: 100,
-      });
+      await request(app)
+        .post("/account")
+        .set("Authorization", `Bearer ${token}`)
+        .send({
+          Name: "New 2",
+          Category: "Bank account",
+          Balance: 100,
+        });
 
-      const after = await request(app).get("/accounts");
+      const after = await request(app)
+        .get("/accounts")
+        .set("Authorization", `Bearer ${token}`);
 
       expect(after.body.length).toBe(before.body.length + 1);
     });
 
     test("should update total Balance", async () => {
-      const before = await request(app).get("/balances");
+      const before = await request(app)
+        .get("/balances")
+        .set("Authorization", `Bearer ${token}`);
 
       const req = {
         Name: "New account3",
@@ -76,9 +114,14 @@ describe("POST /account", () => {
         Balance: 100,
       };
 
-      await request(app).post("/account").send(req);
+      await request(app)
+        .post("/account")
+        .set("Authorization", `Bearer ${token}`)
+        .send(req);
 
-      const after = await request(app).get("/balances");
+      const after = await request(app)
+        .get("/balances")
+        .set("Authorization", `Bearer ${token}`);
 
       expect(after.body.total).toBe(before.body.total + req.Balance);
     });
@@ -86,20 +129,26 @@ describe("POST /account", () => {
 
   describe("negative tests", () => {
     test("should respond with status 422 - input is empty string", async () => {
-      const res = await request(app).post("/account").send({
-        Name: "New 3",
-        Category: "",
-        Balance: 100,
-      });
+      const res = await request(app)
+        .post("/account")
+        .set("Authorization", `Bearer ${token}`)
+        .send({
+          Name: "New 3",
+          Category: "",
+          Balance: 100,
+        });
 
       expect(res.statusCode).toEqual(422);
     });
 
     test("should respond with status 422 - input is missing", async () => {
-      const res = await request(app).post("/account").send({
-        Name: "New 4",
-        Balance: 100,
-      });
+      const res = await request(app)
+        .post("/account")
+        .set("Authorization", `Bearer ${token}`)
+        .send({
+          Name: "New 4",
+          Balance: 100,
+        });
 
       expect(res.statusCode).toEqual(422);
     });
