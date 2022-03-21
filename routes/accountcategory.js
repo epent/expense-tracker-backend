1027b74476ad028378b108ce486f2bbf40c460d0
epent <pentium95@mail.ru>
2022-03-21 16:23:42 +0200

    add token middleware to accounts, balances routes

--- routes/accountcategory.js
+++ routes/accountcategory.js
@@ -1,14 +1,15 @@
 const express = require("express");
 
 const accountcategoryController = require("../controllers/accountcategory");
+const verifyToken = require("../middleware/verify_token");
 
 const router = express.Router();
 
-router.post("/account", accountcategoryController.postAccount);
+router.post("/account", verifyToken, accountcategoryController.postAccount);
 
 router.post("/category", accountcategoryController.postCategory);
 
-router.get("/accounts", accountcategoryController.getAccounts);
+router.get("/accounts", verifyToken, accountcategoryController.getAccounts);
 
 router.get("/categories", accountcategoryController.getCategories);
 
--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,10 @@
 const express = require("express");
 
 const homeController = require("../controllers/home");
+const verifyToken = require("../middleware/verify_token");
 
 const router = express.Router();
 
-router.get("/balances", homeController.getBalances);
+router.get("/balances", verifyToken, homeController.getBalances);
 
 module.exports = router;
