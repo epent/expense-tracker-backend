f1b479981fd4bf16376176474eabf450220b5441
epent <pentium95@mail.ru>
2022-03-22 12:37:11 +0200

    add second demo-balance

--- db/seeders/20220314144705-demo-balances.js
+++ db/seeders/20220314144705-demo-balances.js
@@ -14,6 +14,15 @@ module.exports = {
           updatedAt: new Date(),
           userId: "4cc5127e-0b96-4d3e-a86b-f8a9cf03d0a6",
         },
+        {
+          name: "total_balances2",
+          total: 0,
+          income: 0,
+          expenses: 0,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          userId: "ca1cbe4e-ce5d-4a05-afc4-023735d18772",
+        },
       ],
       {}
     );
